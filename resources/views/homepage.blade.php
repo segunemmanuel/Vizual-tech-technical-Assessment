6bc329d3a9cc9c0cc82e301289a691350fd1bb94
segun <segunemmanuel46@gmail.com>
2022-03-06 11:45:26 -0800

    First commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,99 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="page-header">
+                <p class="page-title">Featured Books</p>
+            </div>
+        </div>
+
+    </div>
+
+    <section id="books">
+<div class="container">
+    <div class="row">
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Book Title:</h5>
+                            <p class="card-text">Author Names:</p>
+                        </div>
+                        <div class="card-footer">
+                    Publishers:
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Book Title</h5>
+                            <p class="card-text">Author Names:</p>
+                        </div>
+                        <div class="card-footer">
+                    Publishers:
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Book Title:</h5>
+                            <p class="card-text">Author Names:</p>
+                        </div>
+                        <div class="card-footer">
+                    Publishers:
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {{-- End of First Row of Books --}}
+            <div class="row">
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Book Title:</h5>
+                            <p class="card-text">Author Names:</p>
+                        </div>
+                        <div class="card-footer">
+                    Publishers:
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Book Title:</h5>
+                            <p class="card-text">Author Names:</p>
+                        </div>
+                        <div class="card-footer">
+                    Publishers:
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-6 col-lg-4 mb-5">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Book Title:</h5>
+                            <p class="card-text">Author Names:</p>
+                        </div>
+                        <div class="card-footer">
+                    Publishers:
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+
+
+
+
+
+@endsection
---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,23 @@
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <a class="navbar-brand text-white" href="#">Bookshop</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="container">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link text-white" href="#">Books</a>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link text-white" href="#">Authors</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link text-white" href="#">Publishers</a>
+          </li>
+        </div>
+      </ul>
+    </div>
+  </nav>
